130634d2e72ffc8697ca37dc8f04da2846a3bc2e
Elizabeth Mashilo <98324930+LizzMash@users.noreply.github.com>
2022-09-13 13:41:37 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""extend your Python script to export data in the JSON format"""
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
-    """Your code should not be executed when imported"""
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user_id = argv[1]
-
-    todos = requests.get(
-        "http://jsonplaceholder.typicode.com/todos?userId={}".format(
-            user_id))
-    user = requests.get(
-        "http://jsonplaceholder.typicode.com/users/{}".format(
-            user_id))
-
-    out = {user.json().get('id'): []}
-    with open('{}.csv'.format(user_id), "w") as output:
-        for tarea in todos.json():
-            data = {
-                'task': tarea.get('title'),
-                'completed': tarea.get('completed'),
-                'username': user.json().get('username')
-            }
-            out.get(user.json().get('id')).append(data)
-        json.dump(out, output)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
